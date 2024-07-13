84df45ee053f9e7d6deca39b9b958d6bfb54639a
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-07-13 23:48:33 +0530

    feat: implement user signup

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,4 +7,10 @@ import {User} from "./user.entity";
 export class UsersService {
     constructor(@InjectRepository(User) private repo: Repository<User>) {
     }
+
+    create(email: string, password: string) {
+        const user = this.repo.create({email, password});
+
+        return this.repo.save(user)
+    }
 }
